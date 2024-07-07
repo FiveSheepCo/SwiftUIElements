c561c3eb6cd511a10d16c2ea5d922f24113702ad
SplittyDev <splittydev@gmail.com>
2024-07-08 01:44:19 +0700

    Add macOS `Blur` view implementation

--- Sources/SwiftUI/Views/Visual Effects/Blur.swift
+++ Sources/SwiftUI/Views/Visual Effects/Blur.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 
+#if canImport(UIKit)
+
 /// A view that provides a blur effect.
 public struct Blur: UIViewRepresentable {
     
@@ -28,3 +30,39 @@ public struct Blur: UIViewRepresentable {
         uiView.effect = UIBlurEffect(style: style)
     }
 }
+
+#elseif canImport(AppKit)
+
+/// A view that provides a blur effect for macOS.
+public struct Blur: NSViewRepresentable {
+    
+    /// The material of the blur effect.
+    private var material: NSVisualEffectView.Material
+    
+    /// Initializes the `Blur` view with a specified blur effect material.
+    /// - Parameter material: The material of the blur effect.
+    public init(material: NSVisualEffectView.Material) {
+        self.material = material
+    }
+    
+    /// Creates the view object and configures its initial state.
+    /// - Parameter context: The context in which the view is being created.
+    /// - Returns: A configured `NSVisualEffectView` with the specified blur effect.
+    public func makeNSView(context: Context) -> NSVisualEffectView {
+        let view = NSVisualEffectView()
+        view.material = material
+        view.blendingMode = .behindWindow
+        view.state = .followsWindowActiveState
+        return view
+    }
+    
+    /// Updates the state of the specified view with new information from SwiftUI.
+    /// - Parameters:
+    ///   - nsView: The view object to update.
+    ///   - context: The context containing the new information.
+    public func updateNSView(_ nsView: NSVisualEffectView, context: Context) {
+        nsView.material = material
+    }
+}
+
+#endif
