479eeef2172465af699b69368c3befe4bd4cedd2
SplittyDev <splittydev@protonmail.com>
2026-01-16 10:12:51 +0700

    Add apply modifiers for views and toolbar contents

---
+++ Sources/SwiftUI/ViewModifiers/Compatibility/Apply.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+extension View {
+
+    /// Applies a modification to the view using a closure.
+    ///
+    /// - Warning: Use sparingly, as this can mess with view identity and performance optimizations.
+    func apply(
+        @ViewBuilder _ modifyContent: (Self) -> some View
+    ) -> some View {
+        modifyContent(self)
+    }
+}
+
+@available(iOS 14, *)
+extension ToolbarContent {
+
+    /// Applies a modification to the toolbar content using a closure.
+    ///
+    /// - Warning: Use sparingly, as this can mess with view identity and performance optimizations.
+    func apply(
+        @ToolbarContentBuilder _ modifyContent: (Self) -> some ToolbarContent
+    ) -> some ToolbarContent {
+        modifyContent(self)
+    }
+}
+
+@available(iOS 14, *)
+extension CustomizableToolbarContent {
+
+    /// Applies a modification to the toolbar content using a closure.
+    ///
+    /// - Warning: Use sparingly, as this can mess with view identity and performance optimizations.
+    func apply(
+        @ToolbarContentBuilder _ modifyContent: (Self) -> some CustomizableToolbarContent
+    ) -> some CustomizableToolbarContent {
+        modifyContent(self)
+    }
+}
